8effea4418c7a7d2850b1ddb289bc3783a0479f1
practice_everything <scre5391@gmail.com>
2024-01-17 21:15:50 +0900

    Introduce Project setup
    firebase/vite/react-dom/styled-component install 했습니다~

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7kk9-oqm8fB3-wTIA7MmwIvJuSgljZSM",
+  authDomain: "introduce-project-community.firebaseapp.com",
+  projectId: "introduce-project-community",
+  storageBucket: "introduce-project-community.appspot.com",
+  messagingSenderId: "22519940920",
+  appId: "1:22519940920:web:4b295f0159ca0af462a3cd",
+  measurementId: "G-1XYW4J5GQS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
